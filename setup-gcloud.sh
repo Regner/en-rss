3eae8a8d60ce1bb76eed14acea1cc9948d811fa3
Regner Blok-Andersen <shadowdf@gmail.com>
2016-02-10 07:06:18 +0000

    derp, forgot to add the actual setting of GCLOUD_ZONE

--- setup-gcloud.sh
+++ setup-gcloud.sh
@@ -5,6 +5,7 @@ set -e
 echo $CLIENT_SECRET | base64 --decode > ${HOME}/client-secret.json
 gcloud auth activate-service-account --key-file ${HOME}/client-secret.json
 gcloud config set project $GCLOUD_PROJECT
+gcloud config set compute/zone $GCLOUD_ZONE
 gcloud config set container/cluster $CLUSTER_NAME
 gcloud container clusters get-credentials $CLUSTER_NAME
 gcloud docker --authorize-only
