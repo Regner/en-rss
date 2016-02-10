6564d67b05ff749d6fbd4f01225aa6c8571fa5a1
Regner Blok-Andersen <shadowdf@gmail.com>
2016-02-10 11:35:26 +0000

    lets just delete and create a new RC instead

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,5 @@ gcloud config set container/cluster $1
 gcloud container clusters get-credentials $1
 
 envsubst < $2 > tmp-rc.yml
-./kubernetes/cluster/kubectl.sh rolling-update $3 -f tmp-rc.yml
+./kubernetes/cluster/kubectl.sh delete rc $3
+./kubernetes/cluster/kubectl.sh create -f tmp-rc.yml
