4097daedb39aeac15e4f725d96700fd55ad792c3
Regner Blok-Andersen <shadowdf@gmail.com>
2016-02-10 11:46:58 +0000

    moving configs and scripts to seperate repo

--- docker-push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker build -t $DOCKER_IMAGE_NAME:$CIRCLE_SHA1 .
-docker tag -f $DOCKER_IMAGE_NAME:$CIRCLE_SHA1 $DOCKER_IMAGE_NAME:latest
-gcloud docker push $DOCKER_IMAGE_NAME:$CIRCLE_SHA1
-gcloud docker push $DOCKER_IMAGE_NAME:latest
--- setup-gcloud.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo $CLIENT_SECRET | base64 --decode > ${HOME}/client-secret.json
-gcloud auth activate-service-account --key-file ${HOME}/client-secret.json
-gcloud config set project $GCLOUD_PROJECT
-gcloud config set compute/zone $GCLOUD_ZONE
-gcloud docker --authorize-only
